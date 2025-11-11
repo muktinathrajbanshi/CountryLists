783c0c4e3c2549869bea8d21e6fa5ab9d6cda253
unknown <muktinathrajbanshi786@gmail.com>
2025-11-11 15:14:34 +0200

    creating error page haha

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AppLayout } from "./components/Layout/AppLayout";
 
 import "./App.css";
+
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Country } from "./pages/Country";
 import { Contact } from "./pages/Contact";
-
+import { ErrorPage } from "./pages/ErrorPage";
 
 
 const router = createBrowserRouter([
   {
     path:"/",
-    element:<Home />
-  },
-  {
-    path:"about",
-    element:<About />
-  },
-  {
-    path:"country",
-    element:<Country />
-  },
-  {
-    path:"contact",
-    element:<Contact />
-  }
+    element:<AppLayout />,
+    errorElement: <ErrorPage />,
+    children:[
+      { 
+    path:"/",
+    element:<Home />,
+    },
+    {
+      path:"about",
+      element:<About />,
+    },
+    {
+      path:"country",
+      element:<Country />,
+    },
+    {
+      path:"contact",
+      element:<Contact />,
+    }
+      ]
+    }
+  
 ]);
 
 const App = () => {
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,9 @@
+import { useRouteError } from "react-router-dom";
+
+export const ErrorPage = () => {
+    const error = useRouteError()
+    console.log(error);
+    
+
+    return <h1>Error Page</h1>
+};
