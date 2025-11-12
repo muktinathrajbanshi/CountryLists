536a5a152d8b70b10bddcb744ca704e899bacefd
unknown <muktinathrajbanshi786@gmail.com>
2025-11-12 13:00:31 +0200

    now this is the time for hero section i mean main section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
 
 export const Home = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <main className="hero-section main">
+      <div className="container grid grid-two-cols">
+
+      <div className="hero-content">
+        <div className="hero-image">
+          
+        </div>
+      </div>
+
+      </div>
+    </main>
+  )
 };
 
