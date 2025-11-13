f4a15693e6cbc86889f3a854c4242d008438cffa
unknown <muktinathrajbanshi786@gmail.com>
2025-11-13 09:50:22 +0200

    wow contact page logic is created for data fetch but design part is not ready

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,18 @@
 
 export const Contact = () => {
+  const handleFormSubmit = (formData) => {
+    // console.log(formData.entries());
+    const formInputData = Object.fromEntries(formData.entries())
+    console.log(formInputData);
+    
+  };
+
   return (
     <section className="section-contact">
       <h2 className="container-title">Contact Us</h2>
 
       <div className="contact-wrapper container">
-      <form action="">
+      <form action={handleFormSubmit}>
         <input 
         type="text" 
         name="username" 
