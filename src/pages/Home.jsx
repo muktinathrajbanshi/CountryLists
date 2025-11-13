35528910ff8de2946ac2ac84fd6494cbbe4710e3
unknown <muktinathrajbanshi786@gmail.com>
2025-11-13 08:16:51 +0200

    in home page align by two other pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import { HeroSection } from "../components/UI/HeroSection";
+import { About } from "./About";
 
 export const Home = () => {
   return (
+    <>
    <HeroSection />
-  )
+   <About />
+    </>
+  );
 };
 
