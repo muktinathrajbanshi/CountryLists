d045e4e2aa1a2202eb95517ae182272895d09bc8
unknown <muktinathrajbanshi786@gmail.com>
2025-11-13 09:39:28 +0200

    wow contact page structure is build

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,42 @@
 
 export const Contact = () => {
-  return <h1>Contact Page</h1>;
+  return (
+    <section className="section-contact">
+      <h2 className="container-title">Contact Us</h2>
+
+      <div className="contact-wrapper container">
+      <form action="">
+        <input 
+        type="text" 
+        name="username" 
+        className="form-control"
+        placeholder="Enter your name"
+        autoComplete="false"
+        required  
+        />
+
+        <input 
+        type="email" 
+        name="email" 
+        className="form-control"
+        placeholder="Enter your email"
+        autoComplete="false"
+        required  
+        />
+
+        <textarea
+        className="form-control"
+        rows="10"
+        placeholder="Enter your message"
+        name="message"
+        required
+        autoComplete="false"
+        ></textarea>
+
+        <button type="submit" value="send">Send</button>
+      </form>
+      </div>
+    </section>
+  );
 };
 
