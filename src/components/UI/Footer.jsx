4e718dfe8f84b328a091c4a3f937e89c8aef48d9
unknown <muktinathrajbanshi786@gmail.com>
2025-11-14 06:17:03 +0400

    additional info is added for footer section

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -3,6 +3,8 @@ import { IoCallSharp } from "react-icons/io5";
 import { MdPlace } from "react-icons/md";
 import { TbMailPlus } from "react-icons/tb";
 
+import { NavLink } from "react-router-dom";
+
 export const Footers = () => {
 
     const footerIcon = {
@@ -19,7 +21,7 @@ export const Footers = () => {
                 const { icon, title, details } = curData;
                 return (
                     <div className="footer-contact" key={index}>
-                        <div className="icon">{footerIcon.icon}</div>
+                        <div className="icon">{footerIcon[icon]}</div>
                         <div className="footer-contact-text">
                             <p>{title}</p>
                             <p>{details}</p>
@@ -29,6 +31,50 @@ export const Footers = () => {
               })  
             }
             </div>
+
+            <div className="copyright-area">
+                <div className="container">
+                    <div className="grid grid-two-cols">
+                        <div className="copyright-text">
+                            <p>
+                                Copyright &copy; 2025, All Right Reserved 
+                                <NavLink to="https://muktinathrajbanshi.com/" target="_blank">
+                                    MuktinathTech
+                                </NavLink>
+                            </p>
+                        </div>
+
+                        <div className="footer-menu">
+                            <ul>
+                                <li>
+                                    <NavLink to="/">Home</NavLink>
+                                </li>
+
+                                <li>
+                                    <NavLink
+                                        to="https://www.instagram.com/muktinath_rajbanshi/"
+                                        target="_blank"
+                                    >
+                                      Social  
+                                    </NavLink>
+                                </li>
+
+                                <li>
+                                    <NavLink
+                                     to ="https://github.com/muktinathrajbanshi"
+                                    >
+                                    Source Code
+                                    </NavLink>
+                                </li>
+                                <li>
+                                   <NavLink to="/contact">Contact</NavLink> 
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </footer>
     );
 };
