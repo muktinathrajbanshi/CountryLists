7af38885208132815d742499d6cad843bfe637b1
unknown <muktinathrajbanshi786@gmail.com>
2025-11-15 12:37:08 +0530

    wow data is fetched from axios method from API

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,5 +1,21 @@
+import { useEffect, useTransition } from "react";
+import { getCountryData } from "../api/postApi";
 
 export const Country = () => {
-  return <h1>Country Page</h1>;
+
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    startTransition(async () => {
+     const res = await getCountryData();
+     console.log(res);
+    });
+  }, []);
+
+  if(isPending) return <h1>Loading...</h1>;
+
+  // return (
+
+  // );
 };
 
