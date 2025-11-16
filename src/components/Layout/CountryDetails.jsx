0b2587bcc6e03bc36cc5e88b468ca557987edea7
unknown <muktinathrajbanshi786@gmail.com>
2025-11-16 13:02:13 +0530

    wow it's incredible moment

--- src/components/Layout/CountryDetails.jsx
+++ src/components/Layout/CountryDetails.jsx
@@ -14,14 +14,19 @@ export const CountryDetails = () => {
         startTransition(async () => {
          const res = await getCountryIndData(params.id);
          console.log(res);
-         
-         setCountry(res);
+         if(res.status === 200) {
+             setCountry(res.data[0]);
+         }
         });
       }, []);
     
       if(isPending) return <Loader />;
 
     return (
-        <h1>Hello Details</h1>
+        <div className="card country-details-card container">
+            <div className="container-card bg-white-box">
+                <h1>{country.name.official}</h1>
+            </div>
+        </div>
     );
 };
