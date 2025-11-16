913e96d9c3ab42e5df99e69a61f89500f48afa46
unknown <muktinathrajbanshi786@gmail.com>
2025-11-16 12:46:26 +0530

    wow through logic build getting all the metadata of countries

--- src/components/Layout/CountryDetails.jsx
+++ src/components/Layout/CountryDetails.jsx
@@ -1,17 +1,21 @@
 import { useEffect, useState, useTransition } from "react";
 import { useParams } from "react-router-dom";
+import { getCountryIndData } from "../../api/postApi";
+import { Loader } from "../UI/Loader";
 
 export const CountryDetails = () => {
     const params = useParams();
     console.log(params);
 
     const [isPending, startTransition] = useTransition();
-      const [countries, setCountries] = useState([]);
+      const [country, setCountry] = useState([]);
     
       useEffect(() => {
         startTransition(async () => {
-         const res = await getCountryData(params.id);
-         setCountries(res.data);
+         const res = await getCountryIndData(params.id);
+         console.log(res);
+         
+         setCountry(res);
         });
       }, []);
     
