9ad6774af90a86d65560fd7a02e0d6ebea736dac
unknown <muktinathrajbanshi786@gmail.com>
2025-11-16 12:28:02 +0530

    wow new logic is build for read more option

--- src/components/Layout/CountryDetails.jsx
+++ src/components/Layout/CountryDetails.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState, useTransition } from "react";
 import { useParams } from "react-router-dom";
 
 export const CountryDetails = () => {
     const params = useParams();
     console.log(params);
+
+    const [isPending, startTransition] = useTransition();
+      const [countries, setCountries] = useState([]);
     
+      useEffect(() => {
+        startTransition(async () => {
+         const res = await getCountryData(params.id);
+         setCountries(res.data);
+        });
+      }, []);
+    
+      if(isPending) return <Loader />;
+
+    return (
+        <h1>Hello Details</h1>
+    );
 };
