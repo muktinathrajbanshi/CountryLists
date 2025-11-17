f073a12344aa7acb1fb93259d70028ea20fc684d
unknown <muktinathrajbanshi786@gmail.com>
2025-11-17 12:30:03 +0200

    oh my god this is for today i will cover it by tommorow

--- src/components/UI/SearchFilter.jsx
+++ src/components/UI/SearchFilter.jsx
@@ -1,4 +1,18 @@
-export const SearchFilter = () => {
-    return <section className="section-searchFilter
-    container"></section>;
+export const SearchFilter = ({ search, setSearch, filter, setFilter}) => {
+
+    const handleInputChange = (event) => {
+        setSearch(event.target.value)
+    }
+
+    return (
+    <section className="section-searchFilter
+    container">
+        <input 
+        type="text" 
+        placeholder="search" 
+        value={search}
+        onChange={handleInputChange}
+        />
+    </section>
+    );
 }
